fdee8352d1fada8fdd7bb759b1179de11639eb28
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-03-21 03:52:16 +0530

    hamburger rotate with header__hamburger__open class css

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -1,1 +1,9 @@
-console.log('hellow world');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('loaded');
+});
+
+const hamburgerButton$ = $('#hamburgerButton');
+
+hamburgerButton$.on("click", () => {
+    hamburgerButton$.toggleClass('header__hamburger__open');
+})
