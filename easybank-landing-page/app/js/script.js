f629b1126be3c26f097af629d6f24ed13628d623
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-03-21 20:46:13 +0530

    animation overlay added using jquery

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -3,7 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 const header$ = $('#header');
+const overlay$ = $('#overlay');
 
 $('#hamburgerButton').on("click", () => {
     header$.toggleClass('header__hamburger__open');
-})
+
+    if (classInClassNameList(overlay$[0], 'fade-in')) {
+        overlay$.removeClass('fade-in');
+        overlay$.addClass('fade-out');
+    }
+    else {
+        overlay$.removeClass('fade-out');
+        overlay$.addClass('fade-in');
+    }
+});
+
+function classInClassNameList(object, property) {
+    return object.classList.contains(property);
+}
