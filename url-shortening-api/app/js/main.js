ba031b17e675071b33303ca8f1e10b4b0fac6055
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-03-28 03:19:23 +0530

    generate copy shorten link from original link

--- url-shortening-api/app/js/main.js
+++ url-shortening-api/app/js/main.js
@@ -1,7 +1,33 @@
 setTimeout(() => {
-    const shortenButton$ = document.getElementById('shortenButton');
-    console.log(shortenButton$);
-    shortenButton$.addEventListener('click', () => {
-        console.log('clicked');
+    const shortenButton$ = $('#shortenButton');
+    const shortenLink$ = document.getElementById('shortenLink');
+
+    shortenButton$.click(() => {
+        const inputLink = shortenLink$.value;
+        generateShortenUrl(inputLink);
     });
-}, 5000);
+}, 2500);
+
+function generateShortenUrl(inputLink) {
+    fetch(`https://api.shrtco.de/v2/shorten?url=${inputLink}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(ex => console.log(ex));
+}
+
+/*
+{
+    code: "ofvAtX"​,
+    full_share_link: "https://shrtco.de/share/ofvAtX",
+    full_short_link: "https://shrtco.de/ofvAtX",
+    full_short_link2: "https://9qr.de/ofvAtX",
+    full_short_link3: "https://shiny.link/ofvAtX",
+    original_link: "http://amazon.com",
+    share_link: "shrtco.de/share/ofvAtX",
+    short_link: "shrtco.de/ofvAtX",
+    short_link2: "9qr.de/ofvAtX",
+    short_link3: "shiny.link/ofvAtX"
+}
+*/
