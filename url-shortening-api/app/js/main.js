e09ea892b0a440a9095a96b62beff5895330c664
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-03-28 04:11:12 +0530

    add shorten data on successfull response

--- url-shortening-api/app/js/main.js
+++ url-shortening-api/app/js/main.js
@@ -3,18 +3,40 @@ setTimeout(() => {
     const shortenLink$ = document.getElementById('shortenLink');
     const shortenHistory$ = document.getElementById('shorten_history');
 
+    const shortenHistoryList$ = document.getElementsByClassName('shorten_history__list')[0];
+    
+    console.log(shortenHistoryList$);
+
+    // shortenHistoryList$.appendChild()
+
     shortenButton$.click(() => {
         const inputLink = shortenLink$.value;
-        generateShortenUrl(inputLink, shortenHistory$);
+        generateShortenUrl(inputLink, shortenHistory$, shortenHistoryList$);
     });
 }, 2500);
 
-function generateShortenUrl(inputLink, shortenHistory$) {
+function generateShortenListItem(originalLink, shortenLink) {
+    const listItem = document.createElement("li");
+    listItem.classList = 'shorten_history__list_item flex flex-col';
+    listItem.innerHTML =
+    `
+    <span class="original-link">${originalLink}</span>
+    <span class="separator"></span>
+    <span class="shortened-link">${shortenLink}</span>
+    <button class="shorten-copy-btn border-rounded">Copy</button>
+    `
+    return listItem;
+}
+
+function generateShortenUrl(inputLink, shortenHistory$, shortenHistoryList$) {
     fetch(`https://api.shrtco.de/v2/shorten?url=${inputLink}`)
     .then(response => response.json())
+    .then(x => x.result)
     .then(data => {
-        console.log(data);
         shortenHistory$.classList.remove('is-hidden');
+        shortenHistoryList$.appendChild(
+            generateShortenListItem(data.original_link, data.share_link)
+        );
     })
     .catch(ex => console.log(ex));
 }
