8b9a596bdf9193a5fe39e72fb1e0f3bb02775258
ashishgopalhattimare <ashishgopalhattimare@gmail.com>
2022-04-04 02:00:05 +0530

    todo initial setup

---
+++ todo-app/src/components/Todos/TodoItem/TodoItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './TodoItem.scss';
+
+const TodoItem = () => {
+    return (
+        <></>
+    );
+}
+
+export default TodoItem;
---
+++ todo-app/src/components/Todos/Todos.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Todos.scss';
+
+export const Todos = () => {
+    return (
+        <>Todos List</>
+    );
+};
